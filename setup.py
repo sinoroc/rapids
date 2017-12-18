359d1257b7690af0dba8f9c4094a69f9551b9981
sinoroc <sinoroc.code+git@gmail.com>
2017-12-18 22:28:57 +0100

    Update packaging

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-""" Setup script
-"""
+#!/usr/bin/env python
+
+
+""" Setup script """
 
 
 import os
@@ -9,11 +11,14 @@ import setuptools
 
 NAME = 'rapids'
 DESCRIPTION = "REST APIs documented and sensible"
-URL = 'https://github.com/sinoroc/rapids'
 
 
 AUTHOR = 'sinoroc'
 AUTHOR_EMAIL = 'sinoroc.code+python@gmail.com'
+URL = 'https://github.com/sinoroc/rapids'
+
+
+LICENSE = 'Apache-2.0'  # https://spdx.org/licenses/
 
 
 REQUIREMENTS_INSTALL = [
@@ -24,46 +29,25 @@ REQUIREMENTS_INSTALL = [
     'zope.interface',
 ]
 
+
 REQUIREMENTS_PACKAGE = [
     'wheel',
 ]
+
 
 REQUIREMENTS_TEST = [
     'pytest',
     'pytest-pep8',
     'pytest-pylint',
-    'readme_renderer',  # needed for 'python setup.py check --restructuredtext'
+    'readme_renderer',  # for 'setup.py check --restructuredtext'
     'WebTest',
 ]
 
+
 REQUIREMENTS_EXTRAS = {
-    'develop': REQUIREMENTS_PACKAGE + REQUIREMENTS_TEST,
     'package': REQUIREMENTS_PACKAGE,
     'test': REQUIREMENTS_TEST,
 }
-
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(HERE, 'README.rst')) as f:
-    README = f.read()
-with open(os.path.join(HERE, 'CHANGELOG.rst')) as f:
-    CHANGELOG = f.read()
-
-
-LONG_DESCRIPTION = README
-VERSION = CHANGELOG.splitlines()[4]
-
-
-SOURCE_DIRECTORY = 'src'
-PACKAGES = setuptools.find_packages(
-    where=SOURCE_DIRECTORY,
-)
-PACKAGE_DIRECTORIES = {
-    '': SOURCE_DIRECTORY,
-}
-
-
-LICENSE = 'Apache-2.0'  # https://spdx.org/licenses/
 
 
 CLASSIFIERS = [
@@ -82,22 +66,39 @@ CLASSIFIERS = [
 
 
 def _do_setup():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'README.rst')) as file_:
+        readme = file_.read()
+    with open(os.path.join(here, 'CHANGELOG.rst')) as file_:
+        changelog = file_.read()
+
+    long_description = readme
+    version = changelog.splitlines()[4]
+
+    source_directory = 'src'
+    packages = setuptools.find_packages(
+        where=source_directory,
+    )
+    package_directories = {
+        '': source_directory,
+    }
+
     setuptools.setup(
         name=NAME,
-        version=VERSION,
+        version=version,
         # metadata
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
         classifiers=CLASSIFIERS,
         description=DESCRIPTION,
         license=LICENSE,
-        long_description=LONG_DESCRIPTION,
+        long_description=long_description,
         url=URL,
         # options
         extras_require=REQUIREMENTS_EXTRAS,
         install_requires=REQUIREMENTS_INSTALL,
-        package_dir=PACKAGE_DIRECTORIES,
-        packages=PACKAGES,
+        package_dir=package_directories,
+        packages=packages,
     )
     return
 
